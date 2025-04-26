db17c47e5154fd6ba2ccb9083889ee7c3865f8e3
brunaporato <brunaporato@gmail.com>
2025-04-26 10:42:33 -0300

    feat: create quiz page

---
+++ src/app/quiz/page.tsx
@@ -0,0 +1,57 @@
+'use client'
+
+import { useState } from "react"
+import { useAuth } from "@/contexts/AuthContext"
+import { questions } from "@/data/questions"
+
+export default function QuizPage() {
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0)
+  
+  const { completeQuiz } = useAuth()
+
+  const currentQuestion = questions[currentQuestionIndex]
+
+  function handleAnswer() {
+    if (currentQuestionIndex + 1 < questions.length) {
+      setCurrentQuestionIndex((prev) => prev + 1)
+    } else {
+      completeQuiz()
+    }
+  }
+
+  return (
+    <main className="flex flex-col min-h-screen items-center justify-center bg-zinc-900 text-zinc-50 p-4">
+      <div className="max-w-xl w-full space-y-14">
+        <div className="flex flex-col gap-5 md:items-center">
+          <h1 className="text-3xl font-medium flex flex-col">
+            QUIZ
+            <span className="font-extrabold -mt-1">FURIOSO 🔥</span>
+          </h1>
+          <p className="text-sm text-zinc-300">Responda as perguntas para conhecermos mais sobre você.</p>
+        </div>
+
+        <div className="bg-zinc-800 p-6 rounded-2xl space-y-6 border border-zinc-600">
+          <h2 className="text-lg font-semibold">{currentQuestion.question}</h2>
+
+          <div className="flex flex-col gap-4">
+            {currentQuestion.options.map(option => (
+              <button
+                key={option}
+                onClick={() => handleAnswer()}
+                className="bg-zinc-100 border border-zinc-500 text-zinc-900 p-1.5 rounded hover:bg-zinc-300 transition duration-200 cursor-pointer"
+              >
+                {option}
+              </button>
+            ))}
+          </div>
+        </div>
+
+        <p className="text-zinc-400 text-center text-sm">
+          Pergunta {currentQuestionIndex + 1} de {questions.length}
+        </p>
+      </div>
+    </main>
+  )
+
+  
+}
---
+++ src/data/questions.ts
@@ -0,0 +1,17 @@
+export const questions = [
+  {
+    id: 1,
+    question: "Qual é o seu jogo favorito?",
+    options: ['CS:GO', 'Valorant', 'League of Legends', 'Outro'],
+  },
+  {
+    id: 2,
+    question: "Com que frequência você assiste os jogos da FURIA?",
+    options: ['Todo jogo', 'Alguns jogos', 'Só finais importantes', 'Raramente'],
+  },
+  {
+    id: 3,
+    question: 'Qual rede social você mais usa para acompanhar a FURIA?',
+    options: ['Instagram', 'Twitter/X', 'TikTok', 'YouTube'],
+  },
+]
